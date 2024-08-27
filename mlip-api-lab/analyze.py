cb073409d16a5734c3e53ef92d99a5ebaf559a57
obeattie25 <158861849+obeattie25@users.noreply.github.com>
2024-08-27 12:32:23 -0400

    Add files via upload

---
+++ mlip-api-lab/analyze.py
@@ -0,0 +1,54 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
+import os
+
+# Load credentials from environment variables
+endpoint = os.getenv("COMPUTER_VISION_ENDPOINT")
+key = os.getenv("COMPUTER_VISION_KEY")
+
+if not endpoint or not key:
+    raise ValueError("Please set the COMPUTER_VISION_ENDPOINT and COMPUTER_VISION_KEY environment variables.")
+
+credentials = CognitiveServicesCredentials(key)
+
+client = ComputerVisionClient(
+    endpoint=endpoint,
+    credentials=credentials
+)
+
+def read_image(uri):
+    numberOfCharsInOperationId = 36
+    maxRetries = 10
+
+    # SDK call
+    rawHttpResponse = client.read(uri, language="en", raw=True)
+
+    # Get ID from returned headers
+    operationLocation = rawHttpResponse.headers["Operation-Location"]
+    idLocation = len(operationLocation) - numberOfCharsInOperationId
+    operationId = operationLocation[idLocation:]
+
+    # SDK call
+    result = client.get_read_result(operationId)
+    
+    # Try API
+    retry = 0
+    
+    while retry < maxRetries:
+        if result.status.lower () not in ['notstarted', 'running']:
+            break
+        time.sleep(1)
+        result = client.get_read_result(operationId)
+        
+        retry += 1
+    
+    if retry == maxRetries:
+        return "max retries reached"
+
+    if result.status == OperationStatusCodes.succeeded:
+        res_text = " ".join([line.text for line in result.analyze_result.read_results[0].lines])
+        return res_text
+    else:
+        return "error"
